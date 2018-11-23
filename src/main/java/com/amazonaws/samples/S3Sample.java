c5db09015f696825464d8e4438cbfbf970c7101b
Xapollon430 <42275242+Xapollon430@users.noreply.github.com>
2018-11-23 05:32:28 -0800

    Second

--- src/main/java/com/amazonaws/samples/S3Sample.java
+++ src/main/java/com/amazonaws/samples/S3Sample.java
@@ -1,25 +1,9 @@
 package com.amazonaws.samples;
 
-/*
- * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.HttpURLConnection;
@@ -210,17 +194,6 @@ public class S3Sample {
 	 *
 	 * @throws IOException
 	 */
-	private static void displayTextInputStream(InputStream input) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-		while (true) {
-			String line = reader.readLine();
-			if (line == null)
-				break;
-
-			System.out.println("    " + line);
-		}
-		System.out.println();
-	}
 
 	public static Names[] GetMembers() {
 
